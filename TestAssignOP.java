bea1ea7f89b82fd4dfbc06bffe96aa464e099087
yi-ting-chen <today801001@gmail.com>
2020-11-08 14:49:25 +0800

    testttt testttttt

---
+++ TestAssignOP.java
@@ -0,0 +1,16 @@
+package module01_06;
+/*
+ * 此範例為指定運算子測試
+ */
+public class TestAssignOP {
+
+	public static void main(String[] args) {
+		int num = 1;
+		num += 2;    
+		String s = "1";
+		s += 2;  
+		System.out.println(num);
+		System.out.println(s);
+	}
+
+}
